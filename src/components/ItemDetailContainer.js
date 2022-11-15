ec38becbe6278a919b2f35cbe46c160e4f73909f
Fedeherrera <fedeeherrera2001@gmail.com>
2022-11-15 11:54:35 -0300

    Cambios

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import {getProduct} from '../utilities/products';
 import {useParams} from 'react-router-dom'
+//import {itemDetail} from '../container/itemDetail'
 
-const ItemDetailContainer = () => {
+const ItemDetailContainer = (props) => {
   const [product, setProduct] = useState({})
   const {idItem} = useParams();
   useEffect(()=> {
@@ -13,7 +14,11 @@ const ItemDetailContainer = () => {
       console.log({product})
   return (
     <div>
-        <h1>BIENVENIDO A ITEM DETAIL CONTAINER</h1>
+        <h3>{props.nombre}</h3>
+        <h4> ID: {props.id}</h4>
+        <img src={props.imagen} alt="" />
+        <h4> Stock: {props.stock}</h4>
+        <h4> Categoria: {props.category}</h4>
     </div>
   )
 }
---
+++ src/container/itemDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {itemDetailContainer} from '../container/itemDetailContainer'
+
+const itemDetail = (props, id) => { 
+  return (
+    <div>
+      <itemDetailContainer/>
+    </div>
+  )
+}
+
+export default itemDetail
